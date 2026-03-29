934497041f264cc89c110cb9aed9ff16819b0296
Rogier Verkaik <rogierv@gmail.com>
2026-03-29 12:39:11 +0200

    Fix formatting in Terraform provider configuration for Azure AD authentication

--- infra/env/dev/providers.tf
+++ infra/env/dev/providers.tf
@@ -12,7 +12,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
     use_oidc             = true
-    use_azuread_auth     = true 
+    use_azuread_auth     = true
   }
 }
 
